0dfbf09af4d8fc7c3719419449bfdbb9537f8042
Tibaldi De Jesus Gonzalez <phittipaldi@gmail.com>
2017-11-12 11:50:54 -0400

    Unittest for Registration User Form

--- apps/security/tests.py
+++ apps/security/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .forms import RegistrationForm
 
-# Create your tests here.
+
+class UserFormTest(TestCase):
+
+    # Valid Form Data
+    def test_register_form_valid(self):
+        form = RegistrationForm(data={'username': "user@mp.com",
+                                      'full_name': "Tibaldi de Jesus",
+                                      'password1': "tibaldix123",
+                                      'password2': "tibaldix123", })
+        self.assertTrue(form.is_valid())
+
+    # Invalid Form Data
+    def test_register_form_invalid(self):
+        form = RegistrationForm(data={'username': "user@mp.com",
+                                      'full_name': "Tibaldi de Jesus",
+                                      'password1': "tibaldix123",
+                                      'password2': "tibaldix", })
+        self.assertFalse(form.is_valid())
