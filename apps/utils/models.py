c41038ee2281445f0713d839b8faa22fa49acaf1
Tibaldi De Jesus Gonzalez <phittipaldi@gmail.com>
2017-11-27 21:35:19 -0400

    create the basic model for budget and utils apps

---
+++ apps/utils/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class CommonInfo(models.Model):
+    user_insert = models.ForeignKey(
+        User,
+        related_name='user_insert_%(class)s',
+        editable=False)
+    user_update = models.ForeignKey(
+        User,
+        related_name='user_update_%(class)s',
+        blank=True, null=True, editable=False)
+
+    class Meta:
+        abstract = True
+        ordering = ['name']
+
+
+class Color(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Currency(models.Model):
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
