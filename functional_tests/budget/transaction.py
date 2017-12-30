662345810339269403d01c08dfa2fd5e7b45439b
Tibaldi De Jesus Gonzalez <tdejesus@Tibaldis-MacBook-Pro.local>
2017-12-30 10:44:07 -0400

    change template for account managment

--- functional_tests/budget/transaction.py
+++ functional_tests/budget/transaction.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from .base import FunctionalTest
-# from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import Select
 from django.urls import reverse
-# from django.test.utils import override_settings
+from django.test.utils import override_settings
 # from apps.budget.tests.factories import AccountFactory
 
 
@@ -18,3 +18,32 @@ class TransactionTest(FunctionalTest):
             'msg_no_transaction').text
 
         self.assertIn(display_text, 'There are not transactions created')
+
+    @override_settings(DEBUG=True)
+    def test_create_account_success(self):
+
+        url_add_account = reverse('budget:transaction_add')
+        self.browser.get(self.live_server_url + url_add_account)
+        self.login()
+
+        account = Select(self.browser.find_element_by_id('id_account'))
+        id_trx_type = Select(self.browser.find_element_by_id('id_trx_type'))
+        amount = self.browser.find_element_by_id('id_amount')
+        currency = Select(self.browser.find_element_by_id('id_currency'))
+        category = Select(self.browser.find_element_by_id('id_category'))
+        subcategory = Select(self.browser.find_element_by_id('id_subcategory'))
+
+        name.send_keys('Family Account')
+        account_type.select_by_visible_text('General')
+        amount.send_keys('5700')
+        currency.select_by_visible_text('USD')
+
+        log_but2 = "//input[@class='btn btn-success']"
+        self.browser.find_element_by_xpath(log_but2).click()
+
+        self.wait_for_row_in_list_table('Family Account General 5700.00 Edit Delete')
+
+    account = models.ForeignKey(Account)
+    trx_type = models.ForeignKey(TransactionType)
+    subcategory = models.ForeignKey(SubCategory)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
