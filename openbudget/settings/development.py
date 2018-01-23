84e4c429a2e59dbfb9b8082897eb2cda332beae1
Tibaldi <phittipaldi@gmail.com>
2018-01-23 16:57:41 -0400

    add settings module development and production

---
+++ openbudget/settings/development.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+ACCOUNT_EMAIL_REQUIRED = False
+ACCOUNT_EMAIL_VERIFICATION = False
+ACCOUNT_LOGIN_ON_EMAIL_CONFIRMATION = False
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+INTERNAL_IPS = '127.0.0.1'
