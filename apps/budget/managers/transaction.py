34def0c3e6ed17c33ff35f6e6572fecb6ddb37a7
Tibaldi <phittipaldi@gmail.com>
2018-01-26 18:11:58 -0400

    begin modal templates

--- apps/budget/managers/transaction.py
+++ apps/budget/managers/transaction.py
@@ -5,3 +5,12 @@ class TransactionManager(models.Manager):
 
     def all_my_transactions(self, owner):
         return self.filter(account__owners__in=[owner]).order_by('-date')
+
+    def all_my_transactions_by_period(self, owner, period):
+        filter_data = dict()
+        filter_data['date__range'] = [period.init_date, period.end_date]
+        filter_data['account__in'] = [owner]
+        return self.filter(**filter_data).order_by('-date')
+
+    def my_last_transactions(self, owner):
+        return self.filter(account__owners__in=[owner]).order_by('-date')[:0]
