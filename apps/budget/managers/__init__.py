ff903f2a51670a1d00cdc858366e3f00ee36628a
Tibaldi <phittipaldi@gmail.com>
2018-04-16 11:04:21 -0400

    fix success url sinc transaction

--- apps/budget/managers/__init__.py
+++ apps/budget/managers/__init__.py
@@ -1,5 +1,5 @@
 from .account import AccountManager
-from .transaction import TransactionManager
+from .transaction import TransactionManager, TransactionUploadedManager
 from .budget import BudgetManager, BudgetShareManager
 from .category import CategoryManager
 from .currency import CurrencyManager
